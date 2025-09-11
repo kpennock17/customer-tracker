068970641ac16d76a4547223c0d769f3e4e111ac
Kevin Pennock <kpennock@usf.edu>
2025-09-11 19:28:06 -0400

    step 1

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+let customers = [
+    {name:"Carmen",email:"carmenola@gmail.com",purchases:("socks","TV","Ethernet")},
+    {name:"Miguel",email:"migueloncho@gmail.com",purchases:("shoes","Pokemon Card","Plunger")},
+    {name:"Jorge",email:"georginho@gmail.com",purchases:("Apple","Watch","Towel")},
+];
