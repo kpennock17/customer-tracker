1a8515546e52958a98a634bf1a904b19870d6c31
Kevin Pennock <kpennock@usf.edu>
2025-09-14 23:37:06 -0400

    step 5 done

--- cc_3.js
+++ cc_3.js
@@ -3,10 +3,10 @@ let customers = [
     {name:"Miguel",email:"migueloncho@gmail.com",purchases:("shoes","Pokemon Card","Plunger")},
     {name:"Jorge",email:"georginho@gmail.com",purchases:("Apple","Watch","Towel")},
 ];
-customers.push({name:"Zach",email:"Zarchman@gmail.com",purchases:("Football","Anime","Glasses"})
-customers.shift("Carmen")
+customers.push({name:"Zach",email:"Zarchman@gmail.com",purchases:("Football","Anime","Glasses")});
+customers.shift("Carmen");
 
-customers[0].email = "miguelthebell@gmail.com"
-customers[3].purchases.push =("Bag")
+customers[0].email = "miguelthebell@gmail.com";
+customers[3].purchases.push =("Bag");
 
 customers.forEach(customer => {console.log(`${customer.name} | Email: ${customer.email} | Total Purchases: ${customer.purchases.length}`)});
